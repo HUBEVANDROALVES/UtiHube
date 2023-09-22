e97a450d46d9419d3a78aa8df6edae49809cc566
hubevandro <hubevandro.alves@ufpe.br>
2023-09-22 17:20:12 -0300

    modificacoes feita no aquivo, em 21.09

--- musicAmbient/ContentView.swift
+++ musicAmbient/ContentView.swift
@@ -8,43 +8,54 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
+    @StateObject private var audioManager = AudioManager()
+    
+    var body: some View {
+        Sound()
+            .environmentObject(audioManager)
+    }
+}
+
+struct Sound: View {
+    @EnvironmentObject var audioManager: AudioManager
+    var body: some View {
+        VStack {
+            Button(action: {
+                if audioManager.isPlaying {
+                    audioManager.audioPlayer?.pause()// para a musica
+                } else {
+                    audioManager.audioPlayer?.play()// toca a musica
+                }
+                audioManager.isPlaying.toggle() // intercarla para e toca
+                }){
+                HStack {
+                    Image(audioManager.isPlaying ? "speaker" : "speakStop")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
+                
+            }
+        }
+    }
+}
+class AudioManager: ObservableObject {
     var audioPlayer: AVAudioPlayer?
-    @State private var isPlaying = true
+    @Published var isPlaying = true
     
     init() {
+        NotificationCenter.default.addObserver(self, selector: #selector(pauseSong), name: UIApplication.didEnterBackgroundNotification, object: nil)
         let musicURL = Bundle.main.url(forResource: "music", withExtension: "mp3")
+        
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: musicURL!)
-            audioPlayer?.play() //  ja inicia tocando
-            
-    
+            // audioPlayer?.play() //  ja inicia tocando
         } catch {
             print(error)
         }
     }
     
-    var body: some View {
-        VStack {
-            Button(action: {
-                if isPlaying {
-                    audioPlayer?.pause()// para a musica
-                } else {
-                    audioPlayer?.play()// toca a musica
-                }
-                isPlaying.toggle() // intercarla para e toca
-                
-            }) {
-                HStack {
-                    Image(isPlaying ? "speaker" : "speakStop" )
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                }
-            }
-        }
-    }
-        func onDisappear() {
-                audioPlayer?.stop()
-        
+    @objc func pauseSong() {
+        audioPlayer?.stop()
     }
 }
 
